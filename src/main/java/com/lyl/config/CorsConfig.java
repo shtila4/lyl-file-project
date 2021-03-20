7caa68ba160eaa4cc10fc52c9bbcf0bff9917b1c
shtila4 <70467359+shtila4@users.noreply.github.com>
2021-03-20 17:13:22 +0800

    Update CorsConfig.java
    解决跨域问题

--- src/main/java/com/lyl/config/CorsConfig.java
+++ src/main/java/com/lyl/config/CorsConfig.java
@@ -7,11 +7,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+//     @Override
+//     public void addCorsMappings(CorsRegistry registry) {
+//         registry.addMapping("/*").allowedOrigins("*").
+//                 allowCredentials(true).allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH").maxAge(3600);
+//     }
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*").allowedOrigins("*").
-                allowCredentials(true).allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH").maxAge(3600);
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .maxAge(1800)
+                .allowedOrigins("*");
     }
-
 
 }
